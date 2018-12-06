23054421061cac9b841fba42f275c723fb23591e
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2018-12-06 17:27:57 +0700

    add Courier Seeder:

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call('ProductsTableSeeder');
         $this->call('CustomersTableSeeder');
         $this->call('ProvincesAndCitiesSeeder');
+        $this->call('CouriersTableSeeder');
     }
 }
