affc9e988c7bf96bec0316c4ff5e701bd8b05f20
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2018-12-11 14:01:18 +0700

    add category and subcategory name in products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,6 @@ class Product extends Model
     protected $primaryKey = 'product_id';
 
     protected $fillable = [
-        'code', 'name', 'category_id', 'sub_category_id', 'brand_id', 'point', 'price', 'weight', 'image', 'description', 'status', 'sold', 'hit_views', 'sizes',
+        'code', 'name', 'category_id', 'category_name', 'sub_category_id', 'sub_category_name', 'brand_id', 'point', 'price', 'weight', 'image', 'description', 'status', 'sold', 'hit_views', 'sizes',
     ];
 }
