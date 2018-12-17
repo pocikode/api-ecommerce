f4938247432452abba1374b7cb68fd8985b52a53
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2018-12-17 10:14:46 +0700

    add stocks column in product table

---
+++ database/migrations/2018_12_17_101259_add_stocks_in_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStocksInProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('stocks', 250)->after('sizes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
